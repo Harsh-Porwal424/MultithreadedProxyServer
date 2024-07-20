4651e280093ca679cd5776e06ab10a4e5d0ce097
Harsh Porwal <harshporwal@Harshs-MacBook-Air.local>
2024-07-20 22:07:59 +0530

    added clients socket

--- proxy_server_with_cache.c
+++ proxy_server_with_cache.c
@@ -52,7 +52,12 @@ int cache_size;
 
 int main(int argc, char *argv[]){
     int client_socketID, client_len;
-    struct sockaddr server_addr, client_addr;
+    //sockaddr is a generic structure for socket addresses. It contains address family, port number, and IP address.
+    //server_addr and client_addr are used to store the address of the server and client respectively
+    //sockaddr_in socket address internet style (IPv4)
+    struct sockaddr_in server_addr, client_addr;
+
+    //initializing semaphore & lock
     sem_init(&semaphore, 0, MAX_CLIENTS);
     pthread_mutex_init(&lock, NULL);
 
@@ -71,6 +76,65 @@ int main(int argc, char *argv[]){
         perror("ERROR in creating a socket");
         exit(1);
     }
+    int reuse = 1;
+    //Configuting the main proxy_socketId to reuse the same address and port
+    //setsockopt function is used to set options on sockets. It consists of setting the SO_REUSEADDR option.
+    if(setsockopt(proxy_socketId, SOL_SOCKET, SO_REUSEADDR, (char *)&reuse, sizeof(reuse)) < 0) {
+        perror("setsockopt failed");
+        exit(1);
+    }
+
+    //cleaning the server_addr structure
+    bzero((char *) &server_addr, sizeof(server_addr));
+    //Initializing the server_addr structure
+    server_addr.sin_family = AF_INET;
+    //htons is used, so that the networks is able to understand the port number in network byte order
+    server_addr.sin_port = htons(port_number);
+    server_addr.sin_addr.s_addr = INADDR_ANY;
+
+    //binding the main proxy_socketId to the server_addr
+    if(bind(proxy_socketId, (struct sockaddr *) &server_addr, sizeof(server_addr)) < 0){
+        perror("ERROR on binding, Port is not available\n");
+        exit(1);
+    }
+    printf("Proxy server is listening... Binding on port: %d\n", port_number);
+    //listening for incoming connections
+
+    int listen_status = listen(proxy_socketId, MAX_CLIENTS);
+
+    if(listen_status < 0){
+        perror("ERROR on listening\n");
+        exit(1);
+    }
+
+    //definnig the iterator.
+    int i = 0;
+    int Connected_socketId[MAX_CLIENTS];
+
+    while(1){
+        bzero((char *) &client_addr, sizeof(client_addr));
+        client_len = sizeof(client_addr);
+
+        //accept is used to accept incoming connections. It blocks until a client connects to the server.
+        client_socketID = accept(proxy_socketId, (struct sockaddr *) &client_addr, (socklen_t *)&client_len);
+        if(client_socketID < 0){
+            printf("Not Able to accept connection\n");
+            exit(1);
+        }
+        else{
+            Connected_socketId[i] = client_socketID;
+        }
+
+        //copying the client_addr pointer to client_pt for easier use
+        struct sockaddr_in * client_pt = (struct sockaddr_in *)&client_addr;
+        struct in_addr ip_addr = client_pt->sin_addr;
+        char str[INET_ADDRSTRLEN];
+        //in_ntop converts an Internet address in network byte order to a printable string in IPv4
+        inet_ntop(AF_INET, &ip_addr, str, INET6_ADDRSTRLEN);
+
+
+    }   
+
  
 
 }
