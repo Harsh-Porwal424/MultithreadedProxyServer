f74aa5d6d2f79182c6066c436e044e45cb5cab8c
Harsh Porwal <harshporwal@Harshs-MacBook-Air.local>
2024-07-21 02:56:00 +0530

    added error message

--- proxy_server_with_cache.c
+++ proxy_server_with_cache.c
@@ -80,6 +80,7 @@ int connectRemoteServer(char* host_addr, int port_number){
 
 }
 
+//handle_request function is responsible for handling each incoming request from a client, if it is present in cache then it will return from the cache otherwise it will fetch element from server & return it.
 int handle_request(int clientSocketId, ParsedRequest *request, char* tempReq){
 
     char* buf = (char *)malloc(sizeof(char)*MAX_BYTES);
@@ -148,7 +149,79 @@ int handle_request(int clientSocketId, ParsedRequest *request, char* tempReq){
         bytes_send = recv(remoteSockerId, buf, MAX_BYTES-1, 0);
     }
 
-    return -1;
+    temp_buffer[temp_buffer_index] = '\0';
+    free(buf);
+    add_cache_element(temp_buffer, strlen(temp_buffer), tempReq);
+    free(temp_buffer);
+    close(remoteSockerId);
+    return 0;
+
+}
+
+
+int checkHTTPVersion(char* msg){
+    int version = -1;
+
+	if(strncmp(msg, "HTTP/1.1", 8) == 0)
+	{
+		version = 1;
+	}
+	else if(strncmp(msg, "HTTP/1.0", 8) == 0)			
+	{
+		version = 1;										// Handling this similar to version 1.1
+	}
+	else
+    {
+		version = -1;
+    }
+	return version;
+}
+
+int sendErrorMessage(int socket, int status_code){
+
+    char str[1024];
+	char currentTime[50];
+	time_t now = time(0);
+
+	struct tm data = *gmtime(&now);
+	strftime(currentTime,sizeof(currentTime),"%a, %d %b %Y %H:%M:%S %Z", &data);
+
+	switch(status_code)
+	{
+		case 400: snprintf(str, sizeof(str), "HTTP/1.1 400 Bad Request\r\nContent-Length: 95\r\nConnection: keep-alive\r\nContent-Type: text/html\r\nDate: %s\r\nServer: VaibhavN/14785\r\n\r\n<HTML><HEAD><TITLE>400 Bad Request</TITLE></HEAD>\n<BODY><H1>400 Bad Rqeuest</H1>\n</BODY></HTML>", currentTime);
+				  printf("400 Bad Request\n");
+				  send(socket, str, strlen(str), 0);
+				  break;
+
+		case 403: snprintf(str, sizeof(str), "HTTP/1.1 403 Forbidden\r\nContent-Length: 112\r\nContent-Type: text/html\r\nConnection: keep-alive\r\nDate: %s\r\nServer: VaibhavN/14785\r\n\r\n<HTML><HEAD><TITLE>403 Forbidden</TITLE></HEAD>\n<BODY><H1>403 Forbidden</H1><br>Permission Denied\n</BODY></HTML>", currentTime);
+				  printf("403 Forbidden\n");
+				  send(socket, str, strlen(str), 0);
+				  break;
+
+		case 404: snprintf(str, sizeof(str), "HTTP/1.1 404 Not Found\r\nContent-Length: 91\r\nContent-Type: text/html\r\nConnection: keep-alive\r\nDate: %s\r\nServer: VaibhavN/14785\r\n\r\n<HTML><HEAD><TITLE>404 Not Found</TITLE></HEAD>\n<BODY><H1>404 Not Found</H1>\n</BODY></HTML>", currentTime);
+				  printf("404 Not Found\n");
+				  send(socket, str, strlen(str), 0);
+				  break;
+
+		case 500: snprintf(str, sizeof(str), "HTTP/1.1 500 Internal Server Error\r\nContent-Length: 115\r\nConnection: keep-alive\r\nContent-Type: text/html\r\nDate: %s\r\nServer: VaibhavN/14785\r\n\r\n<HTML><HEAD><TITLE>500 Internal Server Error</TITLE></HEAD>\n<BODY><H1>500 Internal Server Error</H1>\n</BODY></HTML>", currentTime);
+				  //printf("500 Internal Server Error\n");
+				  send(socket, str, strlen(str), 0);
+				  break;
+
+		case 501: snprintf(str, sizeof(str), "HTTP/1.1 501 Not Implemented\r\nContent-Length: 103\r\nConnection: keep-alive\r\nContent-Type: text/html\r\nDate: %s\r\nServer: VaibhavN/14785\r\n\r\n<HTML><HEAD><TITLE>404 Not Implemented</TITLE></HEAD>\n<BODY><H1>501 Not Implemented</H1>\n</BODY></HTML>", currentTime);
+				  printf("501 Not Implemented\n");
+				  send(socket, str, strlen(str), 0);
+				  break;
+
+		case 505: snprintf(str, sizeof(str), "HTTP/1.1 505 HTTP Version Not Supported\r\nContent-Length: 125\r\nConnection: keep-alive\r\nContent-Type: text/html\r\nDate: %s\r\nServer: VaibhavN/14785\r\n\r\n<HTML><HEAD><TITLE>505 HTTP Version Not Supported</TITLE></HEAD>\n<BODY><H1>505 HTTP Version Not Supported</H1>\n</BODY></HTML>", currentTime);
+				  printf("505 HTTP Version Not Supported\n");
+				  send(socket, str, strlen(str), 0);
+				  break;
+
+		default:  return -1;
+
+	}
+	return 1;
 
 }
 
@@ -227,12 +300,12 @@ void *thread_fn_handle_client(void *socketNew){
                 if(request->host && request->path && checkHTTPVersion(request->version)==1){
                     bytes_send_client = handle_request(socket,request, tempReq);
                     if(bytes_send_client == -1){
-                        sendErrorMessage(socket, 500, "Internal Server Error");
+                        sendErrorMessage(socket, 500);
                         printf("Error in handling request\n");
                     }
                 }
                 else{
-                    sendErrorMessage(socket, 400, "Bad Request");
+                    sendErrorMessage(socket, 400);
                     printf("Invalid request\n");
                 }
             }
